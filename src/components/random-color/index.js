a986f304426d9f74fbe9867e7039bc594f7ef137
CalvinTaw <190220508+AmbitiousCalvin@users.noreply.github.com>
2025-02-15 11:38:37 +0700

    Added RGB TO HEX switcher and vice versa

--- src/components/random-color/index.js
+++ src/components/random-color/index.js
@@ -8,17 +8,46 @@ export default function RandomColor() {
     const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
     let string = "#";
     for (let i = 0; i < 6; i++) {
-      string += hex[Math.floor(Math.random() * hex.length)];
+      string += hex[randomColorUtility(hex.length)];
     }
     setColor(string);
   }
 
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
   function createRandomRgbColor() {
-    let arr = [];
-    for (let i = 0; i < 3; i++) {
-      arr.push(Math.floor(Math.random() * 255));
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+    setColor(`rgb(${r}, ${g}, ${b})`);
+  }
+
+  function changeHexToRgb() {
+    if (type !== "rgb") {
+      const r = parseInt(color.slice(1).slice(0, 2), 16);
+      const g = parseInt(color.slice(1).slice(2, 4), 16);
+      const b = parseInt(color.slice(1).slice(4, 6), 16);
+      setColor(`rgb(${r}, ${g}, ${b})`);
     }
-    setColor(`rgb(${arr.join(", ")})`);
+  }
+
+  function getHex(value) {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    return `${hex[Math.floor(value / 16)]}${Math.floor(
+      ((value % 16) / 100) * 16
+    )}`;
+  }
+
+  function changeRgbToHex() {
+    if (type !== "hex") {
+      const newstr = color.slice(3).split(",");
+      let firstNumber = getHex(newstr[0].slice(1));
+      let secNumber = getHex(newstr[1]);
+      let lastNumber = getHex(newstr[2].slice(0, -1));
+      setColor(`#${firstNumber}${secNumber}${lastNumber}`);
+    }
   }
 
   return (
@@ -30,13 +59,29 @@ export default function RandomColor() {
         background: color,
       }}
     >
-      <button onClick={() => setType("hex")}>HEX COLOR</button>
-      <button onClick={() => setType("rgb")}>RGB COLOR</button>
+      <button
+        onClick={() => {
+          changeRgbToHex();
+          setType("hex");
+        }}
+      >
+        HEX COLOR
+      </button>
+      <button
+        onClick={() => {
+          changeHexToRgb();
+          setType("rgb");
+        }}
+      >
+        RGB COLOR
+      </button>
+      }
       <button
         onClick={type === "hex" ? createRandomHexColor : createRandomRgbColor}
       >
         GENERATE RANDOM COLOR
       </button>
+      <h3>{type.toUpperCase()} COLOR: </h3>
       <h1>{color}</h1>
     </div>
   );
